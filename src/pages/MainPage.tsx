be8a11592fdf046fdd1cda7139722f8e26a09b5f
daaoooy <y020d226y@daum.net>
2026-04-17 04:39:02 +0900

    🔧 Add alias settings

---
+++ src/pages/MainPage.tsx
@@ -0,0 +1,12 @@
+import CasperDongGuImage from "@/assets/casper-donggu-default.png";
+
+const MainPage = () => {
+  return (
+    <div className="flex flex-col h-screen items-center justify-center">
+      <img src={CasperDongGuImage} className="w-60 h-60" />
+      <p>구현 중..</p>
+    </div>
+  );
+};
+
+export default MainPage;
