519aaa2e27be4c7a829225abe3bd0b45396b220c
daaoooy <y020d226y@daum.net>
2026-04-17 14:53:38 +0900

    feat: initial commit

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,10 +1,44 @@
+import { useEffect, useState } from "react";
 import CasperDongGuImage from "@/assets/casper-donggu-default.png";
 
 const MainPage = () => {
+  const [start, setStart] = useState(false);
+  const [shake, setShake] = useState(false);
+
+  useEffect(() => {
+    if (!start) return;
+
+    const interval = setInterval(() => {
+      setShake(true);
+      setTimeout(() => setShake(false), 80);
+    }, 140);
+
+    return () => clearInterval(interval);
+  }, [start]);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setStart(true);
+    }, 300);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
-    <div className="flex flex-col h-screen items-center justify-center">
-      <img src={CasperDongGuImage} className="w-60 h-60" />
-      <p>구현 중..</p>
+    <div className="flex flex-col h-screen items-center justify-center overflow-hidden">
+      <div
+        className={`transition-all duration-7000 ease-linear
+        ${
+          start
+            ? "translate-x-0 translate-y-0 scale-100"
+            : "translate-x-160 -translate-y-140 scale-10"
+        }`}
+      >
+        <img
+          src={CasperDongGuImage}
+          className={`w-70 h-70 scale-100 ${shake ? "translate-y-px" : ""}`}
+        />
+      </div>
     </div>
   );
 };
